a6fc523bfcaf160eece518d0870205c4d012b5eb
Lulu Shi <crazzy_rabbit@163.com>
2023-08-29 15:13:34 +0800

    Update treemix.sh

--- gene_flow/treemix/treemix.sh
+++ gene_flow/treemix/treemix.sh
@@ -1,2 +1,2 @@
-plink2treemixIN="/home/sll/script/treemix/plink2treemixIN.py"
-python $plink2treemixIN --sample 157_cattle_snp_geno01_maf005-ld502502_nchr.order.txt --bed /home/sll/20230818-sll-vcf/structure/NJtree/159_cattle_snp_geno01_maf005 --output tree
+plink2treemixIN="~/script/treemix/plink2treemixIN.py"
+python $plink2treemixIN --sample 159poplist.txt --bed /home/sll/20230818-sll-vcf/structure/NJtree/159_cattle_snp_geno01_maf005_nchr --output tree
