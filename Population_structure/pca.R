f74ba70965f6337b68a9e6605884241927bcd12c
Lulu Shi <crazzy_rabbit@163.com>
2023-08-30 09:30:22 +0800

    Update pca.R

--- Population_structure/pca.R
+++ Population_structure/pca.R
@@ -1,20 +1,20 @@
-  library("ggplot2")
-  a=read.table("PCA_157_cattle_snp_geno01_maf005-ld502502_nchr.gcta.out.eigenvec",header=F)
-  Breed=a[,1] 
+library("ggplot2")
+a=read.table("PCA_157_cattle_snp_geno01_maf005-ld502502_nchr.gcta.out.eigenvec",header=F)
+Breed=a[,1] 
 
-  m_colour=c("ANG"="#6BB93F","HOL"="#6BB93F","SIM"="#6BB93F","SHO"="#6BB93F","CHR"="#6BB93F",
+m_colour=c("ANG"="#6BB93F","HOL"="#6BB93F","SIM"="#6BB93F","SHO"="#6BB93F","CHR"="#6BB93F",
              "MIS"="#18A2CA","KUC"="#18A2CA","WAG"="#E20593","HAW"="#873186","KAZ"="#873186",
              "MON"="#873181","YB"="#873181","WL"="#DBB71D","BRA"="#b35107")
-  m_shape=c("ANG"=21,"HOL"=21,"SIM"=21,"SHO"=21,"CHR"=21,
+m_shape=c("ANG"=21,"HOL"=21,"SIM"=21,"SHO"=21,"CHR"=21,
             "MIS"=25,"KUC"=24,"WAG"=22,"HAW"=21,"KAZ"=21,
             "MON"=21,"YB"=21,"WL"=21,"BRA"=21)
   
-  p = ggplot(data  = a, aes(x = a[,3], y = a[,4],
+p = ggplot(data  = a, aes(x = a[,3], y = a[,4],
             group=Breed,shape=Breed,color=Breed,fill=Breed))+
-      geom_point(size=3,alpha = 0.15,stroke=1) + scale_color_manual(values=m_colour)+ 
+    geom_point(size=3,alpha = 0.15,stroke=1) + scale_color_manual(values=m_colour)+ 
       scale_fill_manual(values=m_colour) +  scale_shape_manual(values=m_shape) 
   
-  p + labs(x = "PC1(12.18%)", y = "PC2(6.82%)") + 
+ p + labs(x = "PC1(12.18%)", y = "PC2(6.82%)") + 
       geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
       geom_vline(xintercept = 0, linetype = "dashed", color = "black") + 
       theme_classic() + # 去除灰色背景及网格线
@@ -22,19 +22,19 @@
       theme(legend.position = "none")
 
 
-  ## wag-hol-ang
-  library("ggplot2")
-  a=read.table("wag-hol-ang.pca.txt",header=F)
-  Breed=a[,1] 
-  m_colour=c("ANG"="#6BB93F","HOL"="#6BB93F","WAG"="#E20593")
-  m_shape=c("ANG"=21,"HOL"=21,"WAG"=22)
+## wag-hol-ang
+library("ggplot2")
+a=read.table("wag-hol-ang.pca.txt",header=F)
+Breed=a[,1] 
+m_colour=c("ANG"="#6BB93F","HOL"="#6BB93F","WAG"="#E20593")
+m_shape=c("ANG"=21,"HOL"=21,"WAG"=22)
 
-  p = ggplot(data  = a, aes(x = a[,3], y = a[,4],
+p = ggplot(data  = a, aes(x = a[,3], y = a[,4],
              group=Breed,shape=Breed,color=Breed,fill=Breed))+
     geom_point(size=3,alpha = 0.15,stroke=1) + scale_color_manual(values=m_colour)+
     scale_fill_manual(values=m_colour) + scale_shape_manual(values=m_shape) 
   
-  p + labs(x = "PC1(31.98%)", y = "PC2(17.90%)") + 
+p + labs(x = "PC1(31.98%)", y = "PC2(17.90%)") + 
     geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
     geom_vline(xintercept = 0, linetype = "dashed", color = "black") +
     
