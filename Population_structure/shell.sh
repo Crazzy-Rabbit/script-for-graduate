7d82444cda5d74857bd18c6ca2616ff955d95f87
Lulu Shi <crazzy_rabbit@163.com>
2023-08-30 09:28:51 +0800

    Update shell.sh

--- Population_structure/shell.sh
+++ Population_structure/shell.sh
@@ -13,16 +13,11 @@ $gcta --grm PCA_157_cattle_snp_geno01_maf005-ld502502_nchr.gcta --pca 10 --out P
 
 ############# admixture with 20 bootstrap #############
 if [ $# -ne 3 ]; then
-  echo "error.. need args"
-  echo "command: bash $0 <mK, 最大的K值, 默认从2开始> <nseed, 1-100内随机数的数量，推荐20> <bed file, bed文件,要绝对路径>"
-  exit 1
+echo "error.. need args"
+echo "command: bash $0 <mK, 最大的K值> <nseed, 推荐20> <bed file, 要绝对路径>"
+exit 1
 fi
-
-nK=$1
-nseed=$2
-bed=$3
-base_dir="`pwd`/admixture"   # 存储输出结果的基本目录
-
+nK=$1; nseed=$2; bed=$3; base_dir="`pwd`/admixture"
 mkdir -p $base_dir
 # 生成20个随机数
 for r in $(seq 1 $nseed); 
