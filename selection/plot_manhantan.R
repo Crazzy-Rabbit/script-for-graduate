f469c84d106bd20c044ae8952ba1687993544d43
Lulu Shi <crazzy_rabbit@163.com>
2023-09-01 11:38:31 +0800

    Update plot_manhantan.R

--- selection/plot_manhantan.R
+++ selection/plot_manhantan.R
@@ -21,3 +21,15 @@ CMplot(data1, plot.type="m", LOG10=F,
        amplify = FALSE, file.output=T, height=5, width = 15,
        ylab = "XP-EHH",
        pch =16, cex =0.5, dpi = 600, file = "jpg",memo = "xpehh")
+
+library(CMplot)
+data1 <- read.table("A_H-Wag.norm.XPEHH",header = T,sep = '\t') 
+color_set2 <- c('#1B2C62', '#4695BC')
+
+CMplot(data1, plot.type="m", LOG10=F,
+       chr.den.col=NULL, col = color_set2,
+       threshold = 0.393674, threshold.col = "red", 
+       threshold.lwd= 2, threshold.lty =1,
+       amplify = FALSE, file.output=T, height=5, width = 15,
+       ylab = expression(paste(italic('F'),st)),
+       pch =16, cex =0.5, dpi = 600, file = "jpg",memo = "fst")
