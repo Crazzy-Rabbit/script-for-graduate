decb1bd27acc447f9e918d93195df10061fbdcd3
Lulu Shi <crazzy_rabbit@163.com>
2023-09-01 11:01:16 +0800

    Update plot_manhantan.R

--- selection/plot_manhantan.R
+++ selection/plot_manhantan.R
@@ -8,5 +8,16 @@ CMplot(data1, plot.type="m", LOG10=F,
        threshold.lwd= 2, threshold.lty =1,
        amplify = FALSE, file.output=T, height=5, width = 15,
        ylab = "-ln(θπ_W / θπ_AH)",
-       #ylab = expression(paste(italic('F'),st)),
        pch =16, cex =0.5, dpi = 600, file = "jpg",memo = "pi")
+
+library(CMplot)
+data1 <- read.table("A_H-Wag.norm.XPEHH",header = T,sep = '\t') 
+color_set2 <- c('#1B2C62', '#4695BC')
+
+CMplot(data1, plot.type="m", LOG10=F,
+       chr.den.col=NULL, col = color_set2,
+       threshold = 1.4956, threshold.col = "red", 
+       threshold.lwd= 2, threshold.lty =1,
+       amplify = FALSE, file.output=T, height=5, width = 15,
+       ylab = "XP-EHH",
+       pch =16, cex =0.5, dpi = 600, file = "jpg",memo = "xpehh")
