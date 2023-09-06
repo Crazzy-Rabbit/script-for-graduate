0fc279f13f2bf546f53142e4d115ad60b84fcb68
Lulu Shi <crazzy_rabbit@163.com>
2023-09-06 20:04:40 +0800

    Update plot_heatmap.R

--- selection/plot_heatmap.R
+++ selection/plot_heatmap.R
@@ -20,3 +20,29 @@ pheatmap(data2, annotation_row=annotation_row, annotation_colors=ra_col,
         cluster_row=FALSE,cluster_col=FALSE,gaps_row=gap_line,legend =FALSE,
         labels_col = rep("",ncol(data2)),labels_row=rep("",nrow(data2)),
         color=c("#F7F8D5","#BF3826"), angle_col = "0")
+
+
+
+library(pheatmap)
+group = read.table("sample_plot.list.txt", stringsAsFactors = F) ##两列，1：ID，2：品种
+names(group) <- c("ID","Group")
+rownames(group) <- group$ID
+group$ID <- NULL
+new_order <- c("Wagyu", "Mishima", "KUC", "Haw", "mon", "kaz", "yb", "ang", "hol", "char", "sho","sim") # 新的组顺序
+group$Group <- factor(group$Group, levels = new_order)
+
+data2=read.table ("4variant.txt", header = TRUE, stringsAsFactors = F) # 转置后的文件
+data2=data.matrix(data2)
+
+annotation_row = data.frame(Group=as.vector(group$Group))
+rownames(annotation_row) = rownames(data2)
+ra_col <- list(Group=c(Wagyu="#873186", Mishima="#E20593",  KUC="#6BB93F",
+                       Haw="#873186", mon="#E20593",  kaz="#6BB93F", yb="#873186",
+                       ang="#E20593", hol="#6BB93F", char="#873186", sho="#E20593",sim="#6BB93F"))
+#"#3364BC", "#000000", "#F37020", "#DBB71D"
+gap_line <- c(21,30,72) # 数量划分，根据图例顺序，分开每组的热图
+
+pheatmap(data2, annotation_row=annotation_row, border_color=NA, annotation_colors=ra_col,
+        cluster_row=FALSE,cluster_col=FALSE,gaps_row=gap_line,legend =FALSE,
+        labels_col = rep("",ncol(data2)),labels_row=rep("",nrow(data2)),
+        color=c("grey","gold","#6BB93F"), angle_col = "0")
