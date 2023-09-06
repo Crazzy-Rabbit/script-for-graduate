6f1fbfcb9f2fac72228039e253ac9dbe6589748a
Lulu Shi <crazzy_rabbit@163.com>
2023-09-06 20:17:01 +0800

    Update plot_heatmap.R

--- selection/plot_heatmap.R
+++ selection/plot_heatmap.R
@@ -36,13 +36,14 @@ data2=data.matrix(data2)
 
 annotation_row = data.frame(Group=as.vector(group$Group))
 rownames(annotation_row) = rownames(data2)
-ra_col <- list(Group=c(Wagyu="#873186", Mishima="#E20593",  KUC="#6BB93F",
-                       Haw="#873186", mon="#E20593",  kaz="#6BB93F", yb="#873186",
-                       ang="#E20593", hol="#6BB93F", char="#873186", sho="#E20593",sim="#6BB93F"))
+ra_col <- list(Group=c(ang="#873186", hol="#E20593", char="#6BB93F", sho="#873186",sim="#E20593",
+                       Haw="#6BB93F", mon="#873186",  kaz="#E20593", yb="#6BB93F",
+                       Mishima="#873186",KUC="#6BB93F",Wagyu="#E20593"))
 #"#3364BC", "#000000", "#F37020", "#DBB71D"
-gap_line <- c(21,30,72) # 数量划分，根据图例顺序，分开每组的热图
+gap_line <- c(21,112,121) # 数量划分，根据图例顺序，分开每组的热图
 
 pheatmap(data2, annotation_row=annotation_row, border_color=NA, annotation_colors=ra_col,
         cluster_row=FALSE,cluster_col=FALSE,gaps_row=gap_line,legend =FALSE,
         labels_col = rep("",ncol(data2)),labels_row=rep("",nrow(data2)),
         color=c("grey","gold","#6BB93F"), angle_col = "0")
+
