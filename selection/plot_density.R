0d0ac51bd7f0798e57ff84a253cf3f172283b1c4
Lulu Shi <crazzy_rabbit@163.com>
2023-09-11 09:27:54 +0800

    Update plot_density.R

--- selection/plot_density.R
+++ selection/plot_density.R
@@ -1,20 +1,23 @@
 library(ggplot2)
 data <- read.table("all.fst", sep="\t", header=T)
 ggplot(data,aes(x=FST, group=Group, fill=Group))+
-  geom_density(alpha=0.3,adjust=1.5)+
+  geom_density(alpha=0.6,linetype=0)+
+  labs(x=expression(paste(italic('F'),st)), y="Density")+
   theme_bw() +
-  theme(legend.position = c(0.9,0.9))
+  theme(legend.position = c(0.6,0.9))
 
 library(ggplot2)
 data <- read.table("all.lnratio", sep="\t", header=T)
 ggplot(data,aes(x=lnratio, group=Group, fill=Group))+
-  geom_density(alpha=0.3,adjust=1.5)+
+  geom_density(trim=TRUE, alpha=0.6,linetype=0)+
+  labs(x="-ln(θπ_W / θπ_AH)", y="Density")+
   theme_bw() +
-  theme(legend.position = c(0.9,0.9))
+  theme(legend.position = c(0.54,0.9))
   
 library(ggplot2)
 data <- read.table("all.XPEHH.txt", sep="\t", header=T)
 ggplot(data,aes(x=XPEHH, group=Group, fill=Group))+
-  geom_density(alpha=0.3,adjust=1.5)+
+  geom_density(alpha=0.6,linetype=0,adjust=1.5)+
+  labs(x="XP-EHH", y="Density")+
   theme_bw() +
-  theme(legend.position = c(0.9,0.9))
+  theme(legend.position = c(0.6,0.9))
