bc221e86aba0abd3edae0d8c9dfdf9832a7f8b18
Lulu Shi <crazzy_rabbit@163.com>
2023-09-21 16:42:16 +0800

    Update plot_variant_distribution.R

--- CNV/plot_variant_distribution.R
+++ CNV/plot_variant_distribution.R
@@ -1,14 +1,30 @@
+# 数量分布
 library(ggplot2)
 df = read.table("wagyu_all_dis.txt", sep='\t', header=T)
 
-ggplot(df, aes(y= count, x = Type, fill = Region))+ ## 使用ggplot2语法
-       geom_bar(stat = "identity", position = "stack",colour="black",width = 0.8)+ ## 添加柱子
-       theme(panel.border = element_blank(),
-             panel.grid.major = element_blank(), 
-             panel.background = element_blank()) + #去除背景    
-       theme(legend.position = "top") + 
+ggplot(df, aes(y= count, x = Type, fill = Region))+
+       geom_bar(stat = "identity", position = "stack")+ 
+  theme_minimal()+     
+  theme(legend.position = "none") + 
        guides(fill = guide_legend( ncol = 4, byrow = F)) +
        theme(axis.text.y = element_text(face="bold"))+
        theme(axis.text.x = element_text(face="bold"))+
-       labs(x="CNVRs type", y="Number of CNVRs") +
-       scale_fill_brewer(palette = "Paired")## 添加柱子颜色
+       labs(x="", y="Number of CNVRs") +
+  scale_y_continuous(expand = c(0,0))+
+  scale_fill_manual(values=m_col)
+
+# 百分比分布
+library(ggplot2)
+df = read.table("wagyu_all_dis.txt", sep='\t', header=T)
+m_col = c("#EEBB47","#93DAD1","#6DB0D7","#9870CB","#C477A6","#2F71A7","#F5E745","#D8793F","#C63581","#6A3D9A")
+ggplot(df, aes(x=Type, y=percent, fill= Region))+
+  geom_bar(stat = "identity")+ 
+  coord_flip()+
+  theme_minimal() +
+  theme(legend.position = "top") + 
+  guides(fill = guide_legend(ncol = 3, byrow = F)) +
+  theme(axis.text.y = element_text(face="bold"))+
+  theme(axis.text.x = element_text(face="bold"))+
+  labs(x="", y="Percentage of CNVRs (%)") +
+  scale_y_continuous(expand = c(0,0))+
+  scale_fill_manual(values=m_col)
