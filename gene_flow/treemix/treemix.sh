f84d94ab3b85f39690cebc7eba7a93b757854d87
Lulu Shi <crazzy_rabbit@163.com>
2023-09-21 16:44:34 +0800

    Delete gene_flow/treemix directory

--- gene_flow/treemix/treemix.sh
+++
@@ -1,2 +0,0 @@
-plink2treemixIN="~/script/treemix/plink2treemixIN.py"
-python $plink2treemixIN --sample 159poplist.txt --bed /home/sll/20230818-sll-vcf/structure/NJtree/159_cattle_snp_geno01_maf005_nchr --output tree
