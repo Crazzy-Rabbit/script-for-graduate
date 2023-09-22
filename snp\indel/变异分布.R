55516c3b9258dd07ab1b1cf06cc64ba454547349
Lulu Shi <crazzy_rabbit@163.com>
2023-09-22 22:08:34 +0800

    Update 变异分布.R

--- snp\indel/变异分布.R
+++ snp\indel/变异分布.R
@@ -19,7 +19,6 @@ ggplot(data,aes(x=length, y=number,fill="#93DAD1", colour="#7290CC"))+
   scale_y_break(c(4000,5000),
                 space=0.2,
                 scales=1,expand=c(0,0))
-
 # freq
 library(ggplot2)
 data = read.table("indel_freq.txt",sep='\t',header=T)
@@ -47,7 +46,6 @@ ggplot(data, aes(x=Chr,y=snp,colour="#7290CC"))+
   labs(x="Chromosome", y="SNPs/Kb")+
   scale_x_continuous(expand=c(0.02,0),
                      breaks = c(seq(0,29, by=2)))
-
 ## tstv
 library(ggplot2)
 data = read.table("tstv.txt",sep='\t',header=T)
@@ -66,3 +64,63 @@ ggplot(data, aes(x=ts,y=count, col=ts,fill=ts))+
   scale_fill_manual(values=m_cols)+
   coord_flip()+
   labs(x="Ts/Tv Type", y="SNPs Count")
+
+## 在基因组位置 500 400
+library(ggplot2)
+library(ggbreak)
+data = read.table("snpindel_vardis.txt",sep='\t',header=T)
+data$Region <- factor(data$Region, level=c("ncRNA_splicing","splicing","UTR5","ncRNA_exonic","UTR3","upstream",
+                                           "downstream","exonic","ncRNA_intronic","intronic","intergenic"))
+m_col = c("#EEBB47","#D8793F","#EA5D2D","#C63581","#349839",
+          "#6A3D9A","#2F71A7","#C477A6","#93DAD1","#9870CB","#6DB0D7")
+
+ggplot(data, aes(x=Region, y=snp, fill=Region))+
+  geom_bar(stat = "identity",position="stack")+
+  scale_y_continuous(labels=function(x) format(x, scientific = FALSE))+
+  scale_fill_manual(values=m_col)+
+  theme_classic()+
+  theme(legend.position="none")+
+  scale_y_break(c(1000,10000),space=0.2,
+                scales=1,expand=c(0,0))+
+  scale_y_break(c(100000,250000),space=0.2,
+                scales=1,expand=c(0,0))+
+  theme(axis.text.x.top=element_blank(),axis.line.x.top=element_blank(),
+        axis.text.x = element_text(face="bold",angle=45,hjust = 1))+
+  theme(axis.text.y.right=element_blank(),axis.ticks.y.right=element_blank(),
+        axis.text.y = element_text(face="bold"))+
+  labs(x="",y="SNPs Number")
+# indel
+ggplot(data, aes(x=Region, y=indel, fill=Region))+
+  geom_bar(stat = "identity",position="stack")+
+  scale_y_continuous(labels=function(x) format(x, scientific = FALSE))+
+  scale_fill_manual(values=m_col)+
+  theme_classic()+
+  theme(legend.position="none")+
+  scale_y_break(c(10000,20000),space=0.2,
+                scales=1,expand=c(0,0))+
+  theme(axis.text.x.top=element_blank(),axis.line.x.top=element_blank(),
+        axis.text.x = element_text(face="bold",angle=45,hjust = 1))+
+  theme(axis.text.y.right=element_blank(),axis.ticks.y.right=element_blank(),
+        axis.text.y = element_text(face="bold"))+
+  labs(x="",y="InDels Number")
+
+### percentage 460 620 
+library(ggplot2)
+library(ggbreak)
+data = read.table("snpindel_percentage.txt",sep='\t',header=T)
+data$type <- factor(data$type, level=c("SNPs","InDels"))
+data$Region <- factor(data$Region, level=c("ncRNA_splicing","splicing","UTR5","ncRNA_exonic","UTR3","upstream",
+                                           "downstream","exonic","ncRNA_intronic","intronic","intergenic"))
+m_col = c("#EEBB47","#D8793F","#EA5D2D","#C63581","#349839",
+          "#6A3D9A","#2F71A7","#C477A6","#93DAD1","#9870CB","#6DB0D7")
+
+ggplot(data,aes(x=per,y=type,group=type,fill=Region))+
+  geom_bar(stat = "identity")+
+  coord_flip()+
+  scale_fill_manual(values=m_col)+
+  theme_minimal()+
+  theme(legend.position="top")+
+  guides(fill=guide_legend(title=NULL,ncol=3, byrow=F,reverse=T))+
+  theme(axis.text.x=element_text(face="bold"))+
+  theme(axis.text.y=element_text(face="bold"))+
+  labs(x="Percentage(%)",y="")
