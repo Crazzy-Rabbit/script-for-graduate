96768b3395caaadec3c64ce2533cdfcc74f08b7f
Lulu Shi <crazzy_rabbit@163.com>
2023-10-10 09:02:27 +0800

    Update plot_cnvheatmap.R

--- CNV/plot_cnvheatmap.R
+++ CNV/plot_cnvheatmap.R
@@ -1,3 +1,4 @@
+setwd("D:/百度网盘同步文件/BaiduSyncdisk/21-24吉大硕士/2021-2024毕业设计/22.09.26-牛-毕业/拷贝数/文章")
 library(pheatmap)
 group = read.table("sample_plot.list.txt", stringsAsFactors = F)
 names(group) <- c("ID","Group")
@@ -9,8 +10,8 @@ data2=data.matrix(data2)
 
 annotation_row = data.frame(Group=as.vector(group$Group))
 rownames(annotation_row) = rownames(data2)
-ra_col <- list(Group=c(WAG="#873186", AH="#E20593"))
-gap_line <- c(21)
+ra_col <- list(Group=c(WAG="#873186", ANG="#E20593", HOL="#6BB93F"))
+gap_line <- c(21,41)
 
 pheatmap(data2, annotation_row=annotation_row, border_color=NA, annotation_colors=ra_col,
         cluster_row=FALSE,cluster_col=FALSE,gaps_row=gap_line,legend =FALSE,
