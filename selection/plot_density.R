70ba8d2c379037b27b92864c57cbdf55eea5a404
Lulu Shi <crazzy_rabbit@163.com>
2023-10-12 15:24:38 +0800

    Update plot_density.R

--- selection/plot_density.R
+++ selection/plot_density.R
@@ -31,3 +31,26 @@ ggplot(data,aes(x=XPEHH, group=Group, fill=Group))+
   scale_y_continuous(expand = c(0,0))+
   theme_bw() +
   theme(legend.position = "top")
+
+
+# method 2  320 470
+plot.new()
+par(mfrow=c(1,3))
+data <- read.table("all.fst", sep="\t", header=T)
+plot(density(data$FST[data$Group=="Simulated"]), col ="blue",
+     xlim=c(0,1),ylim=c(0,8), main="",xlab=expression(paste(italic('F')[ST])))
+lines(density(data$FST[data$Group=="Observed "]), col="red")
+abline(h=0, col="gray")
+
+data <- read.table("all.lnratio", sep="\t", header=T)
+plot(density(data$lnratio[data$Group=="Simulated"]), col ="blue",
+     xlim=c(-2,6),ylim=c(0,1.6), main="",xlab="-ln(θπ_W / θπ_AH)")
+lines(density(data$lnratio[data$Group=="Observed"]), col="red")
+abline(h=0, col="gray")
+
+data <- read.table("all.XPEHH.txt", sep="\t", header=T)
+plot(density(data$XPEHH[data$Group=="Simulated"]), col ="blue",
+     xlim=c(-2,6),ylim=c(0,1.8), main="",xlab="XP-EHH")
+lines(density(data$XPEHH[data$Group=="Observed"]), col="red")
+abline(h=0, col="gray")
+
