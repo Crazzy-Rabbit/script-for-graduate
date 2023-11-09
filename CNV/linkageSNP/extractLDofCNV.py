5c615a976d9cf794c61c8d24b12a95af1a25621e
Lulu Shi <crazzy_rabbit@163.com>
2023-11-09 20:54:25 +0800

    Create extractLDofCNV.py

---
+++ CNV/linkageSNP/extractLDofCNV.py
@@ -0,0 +1,14 @@
+import gzip
+
+r1 = '/home/sll/2023-CNV-wagyu/08.correct_CNV/fst/CNV.29chr.start.region'
+r2 = '/home/sll/2023-CNV-wagyu/08.correct_CNV/fst/CNV.29chr.end.region'
+Ld = '61_cattle_geno01_maf005_nchr.snp-cnv.LD.gz'
+
+with open (r1,'r') as region1, open (r2,'r') as region2, gzip.open(Ld,'rb') as ld:
+    file_region1 = {(line.split()[0], line.split()[2]) for line in region1}
+    file_region2 = {(line.split()[0], line.split()[1]) for line in region2}
+    for line in ld:
+        if (line.split()[0], line.split()[1]) in file_region1:
+            out.write(line)
+        if (line.split()[0], line.split()[1]) in file_region2:
+            out.write(line)
